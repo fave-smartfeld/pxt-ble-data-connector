0c848418d335ce99700c8901c29b500c2b70d897
fave-smartfeld <verena.fastenbauer@smartfeld.ch>
2024-07-26 15:04:02 +0200

    BLE extension created and initial function created

---
+++ ble-extension.ts
@@ -0,0 +1,19 @@
+/**
+* Makecode Bluetooth Low Energy Data Connector Package.
+* created by: Verena Fastenbauer
+*/
+
+/**
+ * Bluetooth Low Energy (BLE) Data Connector
+ */
+//% weight=0 color=#0082FC icon="\uf294" block="BLE Data Connector"
+namespace SmartfeldBLE {
+
+    /**
+     * gets info
+     */
+    //% blockId="getInfo" block="get BLE Info"
+    export function getInfo(): string {
+        return "BLE Data Connector Info";
+    }
+}
